45125a3e7914247fe898bbfd7030e0ea97f24a8b
Phoenix-Falcon <69624716+Phoenix-Falcon@users.noreply.github.com>
2020-09-07 12:47:52 +0530

    Rename script.js to script.js

---
+++ script.js
@@ -0,0 +1,119 @@
+function steph() {
+ document.getElementById("steph")
+  .innerHTML =
+  "'However difficult life may seem ,there is always something you can do and succeed at.' -Stephen       Hawking";
+}
+
+function confu() {
+ document.getElementById("confu")
+  .innerHTML =
+  "'It does not matter how slowly you go as long as you do not stop.' -Confucius";
+}
+
+function walt() {
+ document.getElementById("walt")
+  .innerHTML =
+  "'All our dreams can come true, if we have the courage to pursue them.' -Walt Disney";
+}
+
+function hugo() {
+ document.getElementById("hugo")
+  .innerHTML =
+  "'There is nothing more powerful in the world than the idea that came in time.' -Victor Hugo";
+}
+
+function aris() {
+ document.getElementById("aris")
+  .innerHTML =
+  "'We are what we repeatedly do. Excellence, then, is not an act, but a habit.' -Aristotle"
+}
+
+function helen() {
+ document.getElementById("helen")
+  .innerHTML =
+  "'Never bend your head. Always hold it high. Look the world straight in the eye.' -Helen Keller";
+}
+
+function steve() {
+ document.getElementById("steve")
+  .innerHTML =
+  "'My favourite things in life don't cost any money . It's really clear the the most precious resource we all have is time.' -Steve Jobs";
+}
+
+function tony() {
+ document.getElementById("tony")
+  .innerHTML =
+  "'Setting goals is the first step in turning the invisible into the visible.' -Tony Robbins";
+}
+
+function pablo() {
+ document.getElementById("pablo")
+  .innerHTML =
+  "'He can who thinks he can, and he can't who thinks he can't. This is an inexorable, indisputable law.' -Pablo Picasso";
+}
+
+function nelson() {
+ document.getElementById("nelson")
+  .innerHTML =
+  "'I learned that courage was not the absence of fear, but the triumph over it. The brave man is not he who does not feel afraid, but he who conquers that fear.' -Nelson Mandela";
+}
+
+function seneca() {
+ document.getElementById("seneca")
+  .innerHTML =
+  "'There is no easy way from the earth to the stars.' -Seneca";
+}
+
+function plato() {
+ document.getElementById("plato")
+  .innerHTML =
+  "'Reality is created by the mind, we can change our reality by changing our mind.' -Plato";
+}
+
+function socrates() {
+ document.getElementById("socrates")
+  .innerHTML =
+  "'The secret of happiness, you see, is not found in seeking more, but in developing the capacity to enjoy less.' -Socrates";
+}
+
+function chris() {
+ document.getElementById("chris")
+  .innerHTML =
+  "'You can never cross the ocean unless you have the courage to lose sight of the shore.' -Christopher Columbus";
+}
+
+function jk() {
+ document.getElementById("jk")
+  .innerHTML =
+  "'It is our choices that show what we truly are, far more than our abilities.' -JK Rowling";
+}
+
+function lev() {
+ document.getElementById("lev")
+  .innerHTML =
+  "'Everyone thinks of changing the world, but no one thinks of changing himself.' -Lev Nikolaevich Tolstoy";
+}
+
+function amy() {
+ document.getElementById("amy")
+  .innerHTML =
+  "'The most difficult thing is the decision to act, the rest is merely tenacity.' -Amelia Earhart";
+}
+
+function earl() {
+ document.getElementById("earl")
+  .innerHTML =
+  "'Never give up on a dream just because of the time it will take to accomplish it. The time will pass anyway.' -Earl Nightingale";
+}
+
+function arty() {
+ document.getElementById("arty")
+  .innerHTML =
+  "'The limits of the possible can only be defined by going beyond them into the impossible.' -Arthur C.Clarke";
+}
+
+function whit() {
+ document.getElementById("whit")
+  .innerHTML =
+  "'Keep your face always toward the sunshine—and shadows will fall behind you.' -Walt Whitman";
+}
